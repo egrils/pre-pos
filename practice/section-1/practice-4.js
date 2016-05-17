e776fe0b1b79806909f755be7e97fdc441c9e184
egrils <egrils@users.noreply.github.com>
2016-05-17 16:02:57 +0800

    review 1_4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,30 +1,22 @@
 function collect_same_elements(collection_a, object_b) {
-  var newArray = [];
-  var collection = object_b['value'];
+    var newArray = [];
+    var collection = object_b.value;
 
-  for(var a = 0; a < collection_a.length; a ++){
-    var element = collection_a[a].key;
-    
-    if(isExist(element,collection))
-      newArray.push(element);
+    collection_a.forEach(function (object) {
+        var element = object.key;
 
-  }
-  return newArray;
+        if(isExist(element,collection))
+            newArray.push(element);
+    });
+    return newArray;
 }
 
 function isExist(element,collection){
+    var boolean = false;
+    collection.forEach(function (array_value) {
+        if (array_value === element)
+            boolean = true;
+    });
 
-  for(var i = 0; i < collection.length; i ++) {
-
-    if (collection[i] === element) return true;
-
-    // collection.forEach(function (array_value) {
-    //  if (array_value === element) return true;
-    // });
-
-  }
-  return false;
-
-
+    return boolean;
 }
-
